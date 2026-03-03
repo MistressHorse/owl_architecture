2009e28d1f528a6813b8366ccb40ac0e85768b7f
MistressHorse <sofiazhguleva131@gmail.com>
2026-03-03 23:32:48 +0300

    Update: main with entropy + example log

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from regex import RegexService, RegexResult
-import entropia
+from regex import *
+from entropia import *
 import os
-import re
 
-def logging(log, imp, line, filename):
+def logging(k, line, filename, imp, log, ent=None):
     with open('./audit.log', 'a', encoding='utf-8') as log_file:
-        log_file.write(log, f'Строка: {line}, файл: {filename}. Уровень критичности: {imp}')
+        if imp != None:
+            log_file.write(f'{log} Строка: {line}, файл: {filename}. Уровень критичности: {imp}')
+        else:
+            log_file.write(f'Утечка данных! Строка {k}: {line}, файл: {filename}. Энтропия: {ent}\n')
 
 
 def search_leaks(direct, method):
@@ -13,13 +15,19 @@ def search_leaks(direct, method):
         for filename in files:
             path = os.path.join(root,filename)
             with open(path, 'r') as file:
+                k = 0
                 for line in file:
+                    k+=1
                     if method == 'regex':
-                        dlp_result = RegexService('./rules.json').check_line
+                        dlp_base = RegexService('./rules.json')
+                        dlp_result = dlp_base.check_line
                         if not dlp_result.skip:
-                            logging(dlp_result.log, dlp_result.imp, line, filename)
-                    #if method == 'entropia':
-                        #дописать альтернативный способ
+                            logging(line, filename, dlp_result.imp, dlp_result.log)
+                    if method == 'entropia':
+                        dlp_result = calculate_entropy(line)
+                        if not dlp_result.skip:
+                            logging(k, line, filename, None, None, dlp_result.entropy)
+                        
 
 direct = str(input('Введите директорию, в которой будет осуществляться поиск: '))
-search_leaks(direct, 'regex')
+search_leaks(direct, 'entropia')
