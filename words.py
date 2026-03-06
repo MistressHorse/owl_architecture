bd81b7cf30d214ed7e58177102fa4bddebbd2a16
Mara-hack <zharar.2011@gmail.com>
2026-03-06 20:19:02 +0300

    Update words.py

--- words.py
+++ words.py
@@ -11,10 +11,13 @@ KEYWORDS = [
     (r'(?i)\buser\b', 'Username'),
     (r'(?i)\busername\b', 'Username'),
     (r'(?i)\btoken\b', 'Token'),
-    (r'(?i)\baws_key\b', 'AWS Key'),
+    (r'(?i)\baws_key\b', 'AWS_Key'),
+    (r'(?i)\baws key\b', 'AWS Key'),
+    (r'(?i)\bkey\b', 'Key'),
     (r'(?i)\baws secret\b', 'AWS Secret'),
+    (r'(?i)\bsecret\b', 'Secret'),
     (r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', 'Email'),
-    (r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', 'IP Address'),  # IPv4
+    (r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', 'IP'), # IPv4
 ]
 
 # Функция для фильтрации заведомо ложных срабатываний 
