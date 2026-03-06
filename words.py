3863b6ec7eae72fecc8e226ed547143147e6d6db
Mara-hack <zharar.2011@gmail.com>
2026-03-06 19:59:46 +0300

    Update words.py

--- words.py
+++ words.py
@@ -6,10 +6,15 @@ from pathlib import Path
 KEYWORDS = [
     (r'(?i)\bpassword\b', 'Password'),
     (r'(?i)\bpasswd\b', 'Password'),
-    (r'(?i)\bpwd\b', 'Password',
-    (r'(?i)\blogin\b', 'Login',
+    (r'(?i)\bpwd\b', 'Password'),
+    (r'(?i)\blogin\b', 'Login'),
     (r'(?i)\buser\b', 'Username'),
     (r'(?i)\busername\b', 'Username'),
+    (r'(?i)\btoken\b', 'Token'),
+    (r'(?i)\baws_key\b', 'AWS Key'),
+    (r'(?i)\baws secret\b', 'AWS Secret'),
+    (r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', 'Email'),
+    (r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', 'IP Address'),  # IPv4
 ]
 
 # Функция для фильтрации заведомо ложных срабатываний 
