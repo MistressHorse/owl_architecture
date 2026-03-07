2e996363e0ab7b11795a7a61bc59e2f58e538715
MistressHorse <sofiazhguleva131@gmail.com>
2026-03-07 04:01:24 +0300

    Modes system in main + rules update!

--- entropia.py
+++ entropia.py
@@ -10,6 +10,6 @@ def calculate_entropy(line):
     counts = Counter(line)
     freq = ((i/len(line)) for i in counts.values())
     ent = - sum(f*log(f,2) for f in freq)
-    if ent > 4:
+    if ent > 4.15:
         return EntropyResult(skip=False, entropy=ent)
     return EntropyResult(skip=True, entropy=ent)
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from regex import *
 from entropia import *
+from words import *
 import os
 import re
 import json
 
-def logging(filename, num, line, type, imp, ent, method):
+def clear_all_service_json():
+    with open('./regex_audit.json','w'):
+        pass
+    with open('./entropia_audit.json','w'):
+        pass
+    with open('./keywords_audit.json','w'):
+        pass
+
+def path(filename):
+    return f'./{filename}'
+
+def logging(filename, num, line, type, imp, ent, method, kwd_point=None):
     par_to_json(filename, num, line, type, imp, ent, f'./{method}_audit.json')
 
 def make_key(obj):
@@ -29,12 +41,12 @@ def par_to_json(filename, num, line, type, imp, ent, json_path):
         data = []
     data.append(finding)
     with open(json_path,'w',encoding='utf-8') as f:
-        f.dump(data, f, indent=4, ensure_ascii=False)
+        json.dump(data, f, indent=4, ensure_ascii=False)
 
 #precise - entropy+regex, kwd+regex, kwd+regex+entropy
 #medium - precise, kwd+entropy
 #agressive - medium, regex, entropy
-def precise_mode(r_file, k_file, e_file):
+def precise_mode(r_file, k_file, e_file, service=False):
     with open(f'{r_file}', encoding='utf-8') as f:
         r_data = json.load(f)
     with open(f'{k_file}', encoding='utf-8') as f:
@@ -42,21 +54,88 @@ def precise_mode(r_file, k_file, e_file):
     with open(f'{e_file}', encoding='utf-8') as f:
         e_data = json.load(f)
     
-    e_dict = {make_key(x) for x in e_data}
-    k_dict = {make_key(x) for x in k_data}
+    e_dict = {make_key(x): x for x in e_data}
+    k_dict = {make_key(x): x for x in k_data}
+    seen = set()
     result = []
     for i in r_data:
         key = make_key(i)
         if key in e_dict or key in k_dict:
-            if key in e_dict:
-                merged = {**i, **e_dict(key)}
-                merged['conf'] = 'HIGH'
-            else:
-                merged = {**i, **k_dict(key)}
-                merged['conf'] = 'HIGH'
+            if key in e_dict and key not in seen:
+                merged = {**i, **e_dict[key]}
+                merged['match'] += ['entropy','regex']
+                seen.add(key)
+            if key in k_dict:
+                merged = {**i, **k_dict[key]}
+                merged['match'] += ['regex','keywords']
+                seen.add(key)
             result.append(merged)
-    with open('precise_audit_result.json', 'w', encoding='utf-8') as f:
-        json.dump(result, f, ensure_ascii=False, indent=4)
+    if service == False:
+        with open('precise_audit_result.json', 'w', encoding='utf-8') as f:
+            json.dump(result, f, ensure_ascii=False, indent=4)
+    else:
+        return result
+
+def medium_mode(r_file, k_file, e_file, service=False):
+    with open(f'{r_file}', encoding='utf-8') as f:
+        r_data = json.load(f)
+    with open(f'{k_file}', encoding='utf-8') as f:
+        k_data = json.load(f)
+    with open(f'{e_file}', encoding='utf-8') as f:
+        e_data = json.load(f)
+    k_dict = {make_key(x): x for x in k_data}
+    precise_list = precise_mode(r_file, k_file, e_file, True)
+    result_med = []
+    for i in e_data:
+        key = make_key(i)
+        if key in k_dict:
+            merged = {**i, **k_dict[key]}
+            merged['match'] += ['keywords','entropy']
+            result_med.append(merged)
+    result_d = {}
+    for i in precise_list + result_med:
+        key = make_key(i)
+        result_d[key] = i
+    result = list(result_d.values())
+    if service == False:
+        with open('medium_audit_result.json', 'w', encoding='utf-8') as f:
+            json.dump(result, f, ensure_ascii=False, indent=4)
+    else:
+        return result
+    
+def agressivee_mode(r_file, k_file, e_file):
+    with open(f'{r_file}', encoding='utf-8') as f:
+        r_data = json.load(f)
+    with open(f'{k_file}', encoding='utf-8') as f:
+        k_data = json.load(f)
+    with open(f'{e_file}', encoding='utf-8') as f:
+        e_data = json.load(f)
+    result_d = {}
+    seen = set()
+    
+    for i in r_data:
+        key = make_key(i)
+        if key not in result_d:
+            result_d[key] = i.copy()
+            result_d[key]['match'] = []
+        result_d[key]['match'].append('regex')
+
+    for i in e_data:
+        key = make_key(i)
+        if key not in result_d:
+            result_d[key] = i.copy()
+            result_d[key]['match'] = []
+        result_d[key]['match'].append('entropy')
+
+    for i in k_data:
+        key = make_key(i)
+        if key in result_d:
+            result_d[key]['match'].append('keywords')
+    
+    result = list(result_d.values())
+    with open('agressive_audit_result.json', 'w', encoding='utf-8') as f:
+            json.dump(result, f, ensure_ascii=False, indent=4)
+    
     
 
 def search_leaks(direct, method):
@@ -76,11 +155,15 @@ def search_leaks(direct, method):
                         dlp_result = calculate_entropy(line)
                         if not dlp_result.skip:
                             logging(filename, num, line, None, None, dlp_result.entropy, method)
-                    # if method == 'keywords':
-                        #приравнивание dlp_result и т.д.
+                    if method == 'keywords':
+                        dlp_result = analyze_line(line)
+                        if not dlp_result.skip:
+                            logging(filename, num, line, dlp_result.leak_type, dlp_result.severity, None, method, 1)
                         
 if __name__ == '__main__':
+    clear_all_service_json()
     direct = str(input('Введите директорию, в которой будет осуществляться поиск: '))
     search_leaks(direct, 'regex')
     search_leaks(direct, 'entropia')
     search_leaks(direct, 'keywords')
+    agressivee_mode('./regex_audit.json', './keywords_audit.json', './entropia_audit.json')
