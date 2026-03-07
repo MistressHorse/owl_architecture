52ef86a298d3e7386ca714616fca1b70c51e03f1
Mara-hack <zharar.2011@gmail.com>
2026-03-07 11:39:51 +0300

    Update main.py

--- scanner/main.py
+++ scanner/main.py
@@ -9,8 +9,8 @@ import math
 from collections import Counter
 
 # Загрузка ML модели 
-MODEL_PATH = './scanner/ML/type_classifier.pkl'
-CLASSES_PATH = './scanner/ML/type_classes.pkl'
+MODEL_PATH = './ML/type_classifier.pkl'
+CLASSES_PATH = './ML/type_classes.pkl'
 ml_model = None
 ml_classes = []
 try:
